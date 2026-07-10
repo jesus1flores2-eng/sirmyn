02153321c10e73e2fb7641113a27505128797ec0
jesus1flores2-eng <jesus1flores2@gmail.com>
2026-07-10 17:53:44 -0600

    Eliminado Flask-Migrate completamente para producción

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
-from app.extensions import db, migrate, login_manager
+from app.extensions import db, login_manager
 from config import config_dict
 from app.routes import register_blueprints
 import pytz
@@ -33,7 +33,6 @@ def create_app():
     # Inicializar extensiones
     csrf.init_app(app)
     db.init_app(app)
-    migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,8 @@
+# app/extensions.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-migrate = Migrate()
 login_manager = LoginManager()
 
 @login_manager.user_loader
